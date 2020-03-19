54aad7234088d2e52344b0bb938c2b6bfd98c249
chikeozulumba <o.devcode@gmail.com>
2020-03-19 18:09:59 +0100

    Added redis production config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+console.log(process.env.NODE_ENV);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
 require('dotenv').config();
 
+const additionalConfig = process.env.NODE_ENV === 'production' ? {
+  password: process.env.REDIS_PASSWORD,
+  path: process.env.REDIS_PATH,
+} : {};
+
 const Queue = BullModule.registerQueue({
   name: 'prescription',
   redis: {
     host: process.env.REDIS_HOST,
     port: parseInt(process.env.REDIS_PORT, 10),
+    ...additionalConfig,
   },
 });
 
